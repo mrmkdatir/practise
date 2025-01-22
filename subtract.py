ad7425d94da481bda697b29e1feea8d85be32722
Mahesh Datir <mr.mkdatir@outlook.com>
2025-01-22 11:34:50 +0530

    subtract

---
+++ subtract.py
@@ -0,0 +1,10 @@
+# Get inputs for a and b from the user
+a = float(input("Enter the value of a: "))
+b = float(input("Enter the value of b: "))
+
+# Perform the subtraction
+result = a - b
+
+# Print the result
+print(f"The result of {a} - {b} is: {result}")
+
