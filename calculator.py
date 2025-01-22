7e0d65b35aa476fe8dec0594d149e8cb540457b4
Mahesh Datir <mr.mkdatir@outlook.com>
2025-01-22 11:39:17 +0530

    added subtraction code

---
+++ calculator.py
@@ -0,0 +1,8 @@
+a = float(input("Enter the value of a: "))
+b = float(input("Enter the value of b: "))
+ 
+# Perform the subtraction
+result = a - b
+ 
+# Print the result
+print(f"The result of {a} - {b} is: {result}")
